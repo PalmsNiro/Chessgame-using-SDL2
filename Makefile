8e1dfaf4f75f59e4178aec6e863616c35bf7a5f3
PalmsNiro <sebi.herr13@gmail.com>
2024-06-17 19:21:17 +0200

    SDL_image library hinzugefügt und einen ersten test mit einem schachbrett hintergrund hinzugefügt

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 # Compiler und Flags
 CXX = g++
 CXXFLAGS = -I libraries/include -I src
-LDFLAGS = -L libraries/lib -lmingw32 -lSDL2main -lSDL2
+LDFLAGS = -L libraries/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
 
 # Ziel und Quell-Dateien
 TARGET = main
-SOURCES = src/main.cpp
-OBJECTS = $(SOURCES:src/%.cpp=src/%.o)
+SOURCES = src/main.cpp src/Chessboard.cpp src/Piece.cpp
+OBJECTS = $(SOURCES:.cpp=.o)
 
 # Standardregel
 all: $(TARGET)
@@ -15,16 +15,23 @@ all: $(TARGET)
 $(TARGET): $(OBJECTS)
 	$(CXX) -o $@ $^ $(CXXFLAGS) $(LDFLAGS)
 
-# Regel zum Erzeugen der Objekt-Dateien
-src/%.o: src/%.cpp
+# Regeln zum Erzeugen der Objekt-Dateien
+src/main.o: src/main.cpp
+	$(CXX) -c -o $@ $< $(CXXFLAGS)
+
+src/Chessboard.o: src/Chessboard.cpp
+	$(CXX) -c -o $@ $< $(CXXFLAGS)
+
+src/Piece.o: src/Piece.cpp
 	$(CXX) -c -o $@ $< $(CXXFLAGS)
 
 # Regel zum Aufräumen
 clean:
 	@echo "Cleaning up..."
 	@if [ -f $(TARGET).exe ]; then rm $(TARGET).exe; fi
-# @if [ -f main.o ]; then rm main.o; fi
-	@if ls src/*.o > /dev/null 2>&1; then rm src/*.o; fi
+	@if [ -f src/main.o ]; then rm src/main.o; fi
+	@if [ -f src/Chessboard.o ]; then rm src/Chessboard.o; fi
+	@if [ -f src/Piece.o ]; then rm src/Piece.o; fi
 	@echo "Clean done."
 
 # Phony targets (diese werden immer ausgeführt, auch wenn eine Datei gleichen Namens existiert)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <common.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "common.hpp"
 
 void drawChessboard(SDL_Renderer* renderer) {
     bool isDarkbrown = false;
@@ -21,13 +23,26 @@ void drawChessboard(SDL_Renderer* renderer) {
     }
 }
 
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren) {
+    SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+    if (texture == nullptr) {
+        std::cout << "LoadTexture Error: " << SDL_GetError() << std::endl;
+    }
+    return texture;
+}
+
 int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cout << "Error: " << SDL_GetError() << std::endl;
         return EXIT_FAILURE;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH+200, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     if (window == NULL) {
         std::cout << "Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
@@ -42,6 +57,15 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    SDL_Texture *chessboardTexture = loadTexture("images/board/blue-marble.jpg", renderer);
+    if (chessboardTexture == nullptr) {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
     SDL_Event event;
     bool isRunning = true;
     while (isRunning) {
@@ -51,15 +75,17 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        drawChessboard(renderer);
-
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, chessboardTexture, NULL, NULL);
         SDL_RenderPresent(renderer); // Present the current rendering
 
-        // SDL_Delay(1000 / 60); // Delay to limit the frame rate to 60 FPS
+        SDL_Delay(1000 / 60); // Delay to limit the frame rate to 60 FPS
     }
 
+    SDL_DestroyTexture(chessboardTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    IMG_Quit();
     SDL_Quit();
 
     return EXIT_SUCCESS;
